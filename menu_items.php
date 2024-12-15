fe32b6bd756a20100d383bcd097105d1d59d4f13
k3mosh <cocsmurftzy@gmail.com>
2024-12-15 14:51:49 +0800

    bubble cart

---
+++ menu_items.php
@@ -0,0 +1,26 @@
+<?php
+include 'dbconn.php';
+
+// Fetch menu items
+$query = "SELECT * FROM menu_items";
+$result = $conn->query($query);
+
+$menuItems = [];
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $menuItems[] = [
+            'id' => $row['id'],
+            'name' => $row['name'],
+            'price' => $row['price'],
+            'image' => $row['image_url'],
+            'stock_quantity' => $row['stock_quantity']
+        ];
+    }
+}
+
+// Return JSON response
+header('Content-Type: application/json');
+echo json_encode($menuItems);
+$conn->close();
+?>
