bdcc6d9921fd4bc547dcc1a8c69efd7b1ee42b0f
k3mosh <cocsmurftzy@gmail.com>
2024-12-16 09:57:30 +0800

    test

--- php/signup.php
+++ php/signup.php
@@ -1,5 +1,17 @@
 <?php
-include 'dbconn.php';
+
+// Database connection
+$servername = "srv1632.hstgr.io";
+$username = "u143688490_user";
+$password = "Kyuzumi112";
+$dbname = "u143688490_bubblecart";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
