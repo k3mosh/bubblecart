16f214ba56ef4bb76a0d22e7d139cc2082835de3
k3mosh <cocsmurftzy@gmail.com>
2024-12-16 19:10:47 +0800

    ssssss

--- js/signup.js
+++ js/signup.js
@@ -86,7 +86,7 @@ form.addEventListener('submit', async (e) => {
     formData.append('password', password);
 
     try {
-        const response = await fetch('/php/customer_signup.php', { method: 'POST', body: formData });
+        const response = await fetch('https://khaki-stingray-740394.hostingersite.com/php/customer_signup.php', { method: 'POST', body: formData });
         const result = await response.json();
 
         if (result.success) {
